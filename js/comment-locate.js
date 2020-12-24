9e6bd4b8e3d4d4abd55f211d57876453c5c3659f
Jerry <ykjia2020@gmail.com>
2020-12-24 16:58:10 +0800

    Site updated: 2020-12-24 16:58:05

---
+++ js/comment-locate.js
@@ -0,0 +1,8 @@
+if(window.location.hash){
+  var checkExist = setInterval(function() {
+    if ($(window.location.hash).length) {
+      $('html, body').animate({scrollTop: $(window.location.hash).offset().top-90}, 1000);
+      clearInterval(checkExist);
+    }
+  }, 100);
+}
